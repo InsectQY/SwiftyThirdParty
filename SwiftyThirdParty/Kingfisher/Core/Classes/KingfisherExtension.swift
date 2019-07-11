30cec777d646fecf393f76c70339011e3dd4d2eb
InsectQY <704861917@qq.com>
2019-07-11 15:40:06 +0800

    1

--- SwiftyThirdParty/Kingfisher/Core/Classes/KingfisherExtension.swift
+++ SwiftyThirdParty/Kingfisher/Core/Classes/KingfisherExtension.swift
@@ -7,11 +7,9 @@ import Kingfisher
 
 public extension UIImageView {
 
-    public func qy_setImage(_ URLString: String?, placeholder placeHolderName: String? = nil, options: KingfisherOptionsInfo? = nil, progress: ((_ receivedSize: Int64, _ totalSize: Int64) -> Void)? = nil, completionHandler: ((_ image: Image?, _ error: NSError?, _ cacheType: CacheType, _ imageURL: URL?) -> Void)? = nil) {
+    public func qy_setImage(_ URLString: String?, placeholder: UIImage?, options: KingfisherOptionsInfo? = nil, progress: ((_ receivedSize: Int64, _ totalSize: Int64) -> Void)? = nil, completionHandler: ((_ image: Image?, _ error: NSError?, _ cacheType: CacheType, _ imageURL: URL?) -> Void)? = nil) {
 
         guard let URLString = URLString, let url = URL(string: URLString) else {return}
-
-        let placeholder: UIImage? = placeHolderName == nil ? nil : UIImage(named: placeHolderName!)
 
         kf.setImage(with: url, placeholder: placeholder, options: options, progressBlock: { (receivedSize, totalSize) in
             progress?(receivedSize, totalSize)
@@ -23,11 +21,9 @@ public extension UIImageView {
 
 public extension UIButton {
 
-    public func qy_setBackgroundImage(_ URLString: String?, placeholder placeHolderName: String? = nil, options: KingfisherOptionsInfo? = nil, progress: ((_ receivedSize: Int64, _ totalSize: Int64) -> Void)? = nil, completionHandler: ((_ image: Image?, _ error: NSError?, _ cacheType: CacheType, _ imageURL: URL?) -> Void)? = nil) {
+    public func qy_setBackgroundImage(_ URLString: String?, placeholder: UIImage?, options: KingfisherOptionsInfo? = nil, progress: ((_ receivedSize: Int64, _ totalSize: Int64) -> Void)? = nil, completionHandler: ((_ image: Image?, _ error: NSError?, _ cacheType: CacheType, _ imageURL: URL?) -> Void)? = nil) {
 
         guard let URLString = URLString, let url = URL(string: URLString) else {return}
-
-        let placeholder: UIImage? = placeHolderName == nil ? nil : UIImage(named: placeHolderName!)
 
         kf.setBackgroundImage(with: url, for: state, placeholder: placeholder, options: options, progressBlock: { (receivedSize, totalSize) in
             progress?(receivedSize, totalSize)
@@ -36,11 +32,9 @@ public extension UIButton {
         })
     }
 
-    public func qy_setImage(_ URLString: String?, placeholder placeHolderName: String? = nil, options: KingfisherOptionsInfo? = nil, progress: ((_ receivedSize: Int64, _ totalSize: Int64) -> Void)? = nil, completionHandler: ((_ image: Image?, _ error: NSError?, _ cacheType: CacheType, _ imageURL: URL?) -> Void)? = nil) {
+    public func qy_setImage(_ URLString: String?, placeholder: UIImage?, options: KingfisherOptionsInfo? = nil, progress: ((_ receivedSize: Int64, _ totalSize: Int64) -> Void)? = nil, completionHandler: ((_ image: Image?, _ error: NSError?, _ cacheType: CacheType, _ imageURL: URL?) -> Void)? = nil) {
 
         guard let URLString = URLString, let url = URL(string: URLString) else {return}
-
-        let placeholder: UIImage? = placeHolderName == nil ? nil : UIImage(named: placeHolderName!)
 
         kf.setImage(with: url, for: state, placeholder: placeholder, options: options, progressBlock: { (receivedSize, totalSize) in
             progress?(receivedSize, totalSize)
